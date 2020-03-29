18a9ea68516acfa9a6882653424d9e812226742d
Daniil <daniil.arduino@yandex.ru>
2020-03-29 19:39:21 +0300

    Add ReadFileZ; Change CalculateTime: move to cpp -> add open function convertDateTimeToSec and convertSecToDateTime; Add new class CVector

---
+++ src/CVector.cpp
@@ -0,0 +1,10 @@
+#include "CVector.h"
+
+CVector::CVector()
+{
+}
+
+CVector::~CVector()
+{
+}
+
---
+++ src/CVector.h
@@ -0,0 +1,12 @@
+#ifndef CVECTOR_H
+#define CVECTOR_H
+
+class CVector
+{
+public:
+    CVector();
+    ~CVector();
+
+};
+
+#endif // CVECTOR_H
