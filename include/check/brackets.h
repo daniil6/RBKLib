58087f116bc2c357c85b4a47f8413424911bba32
Daniil <daniil.arduino@yandex.ru>
2020-11-19 07:53:30 +0300

    Rename Brackets to BracketsStack, add BracketsBool, add BracketsCount

--- include/check/brackets.h
+++ include/check/brackets.h
@@ -1,1 +1,27 @@
-bool Brackets(const char* inStr);
+
+/**
+ * @brief Check brackets with use stack
+ * @param inStr - input string
+ * @param count - count error
+ * @return result
+ * @time 1820 ms per 100000
+ */
+bool BracketsStack(const char* inStr, uint8_t& count);
+
+/**
+ * @brief Check brackets with use shift
+ * @param in - input string
+ * @param count - count error
+ * @return result
+ * @time 100 ms per 100000
+ */
+bool BracketsBool(const char* in, int& count);
+
+/**
+ * @brief Check brackets with use counter
+ * @param in - input string
+ * @param countError - count error
+ * @return result
+ * @time 90 ms per 100000
+ */
+bool BracketsCount(const char* in, int& countError);
--- src/check/brackets.cpp
+++ src/check/brackets.cpp
@@ -1,11 +1,14 @@
+#include <cstdint>
 #include <stack>
 
-bool Brackets(const char* inStr)
+bool BracketsStack(const char* inStr, uint8_t& count)
 {
     std::stack<char> brackets;
+    count = 0;
 
     if(inStr != nullptr) {
         do {
+            count++;
             char symbol = *inStr++;
 
             if(symbol == '(')
@@ -24,6 +27,55 @@ bool Brackets(const char* inStr)
         return false;
 
     if(brackets.empty() == true)
+        return true;
+    else
+        return false;
+}
+
+bool BracketsBool(const char* in, int& count)
+{
+    uint64_t result = 1;
+    count = 0;
+
+    if(in != nullptr) {
+        while(*in != '\0') {
+            count++;
+            if(*in == '(')
+                result <<= 1;
+            if(*in++ == ')')
+                result >>= 1;
+            if(result == 0)
+                return false;
+        }
+    }
+
+    if(result == 1)
+        return true;
+    else
+        return false;
+}
+
+bool BracketsCount(const char* in, int& countError)
+{
+    int counter = 0;
+    countError = 0;
+
+    if(in != nullptr) {
+        while(*in != '\0') {
+            countError++;
+            if(*in == '(') {
+                counter++;
+            } else {
+                if(*in == ')')
+                    counter--;
+                if(counter < 0)
+                    return false;
+            }
+            in++;
+        }
+    }
+
+    if(counter == 0)
         return true;
     else
         return false;
