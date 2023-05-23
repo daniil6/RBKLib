dcd0c075752d3f3aca4749bdde66329b5f2ca713
Daniil <daniil.arduino@yandex.ru>
2023-05-23 22:02:29 +0300

    Add compare array

--- include/compare_symbol.h
+++ include/compare_symbol.h
@@ -1,1 +1,3 @@
 bool CompareSymbol(const char a[], const char b[]);
+
+int compare(const char* a, const int& aSize, const char* b, const int& bSize);
--- src/compare_symbol.cpp
+++ src/compare_symbol.cpp
@@ -7,3 +7,18 @@ bool CompareSymbol(const char a[], const char b[])
             return false;
     return true;
 }
+
+int compare(const char* a, const int& aSize, const char* b, const int& bSize)
+{
+    int min = aSize < bSize ? aSize : bSize;
+    int max = aSize > bSize ? aSize : bSize;
+
+    for(int i = 0; i < min; i++)
+        if(a[i] != b[i])
+            return i;
+
+    for(int i = min; i < max; i++)
+        return min;
+
+    return -1;
+}
