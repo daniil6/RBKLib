b271e1bca10060f53e960f45fd856461577df876
Daniil <daniil6@users.noreply.github.com>
2025-06-29 20:05:05 +0300

    Add install debug version to bat file, hide verbose in cmake file

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 @echo CACHE: %CACHE%
 @echo LIBRARY: %LIBRARY%
 
-rd /q/s "%CACHE%\RBKLib\build"
+rd /q/s "%CACHE%\RBKLib\build-release"
+rd /q/s "%CACHE%\RBKLib\build-debug"
 
-cmake -G "MinGW Makefiles" -B %CACHE%/RBKLib/build -DCMAKE_INSTALL_PREFIX=%LIBRARY%/RBKLib -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
+cmake -G "MinGW Makefiles" -B %CACHE%/RBKLib/build-release -DCMAKE_INSTALL_PREFIX=%LIBRARY%/RBKLib -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF
+cmake -G "MinGW Makefiles" -B %CACHE%/RBKLib/build-debug -DCMAKE_INSTALL_PREFIX=%LIBRARY%/RBKLib -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF
